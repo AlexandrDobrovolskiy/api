0261b72526595ab8ab049b54347c7e7f1dc72d50
AlexandrDobrovolsy <sasha732sasha000@gmail.com>
2018-05-09 12:14:12 +0300

    zapustis' pozhaluysta

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000;
+const port = 8080;
 const host = 'cyber-api.herokuapp.com';
 
 const router = require('./routes');
